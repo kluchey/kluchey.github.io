abd65faf7ff3326bf7407c2a4f9cb3606acee07c
kcluchey <kcluchey@dxdy.org>
2016-02-16 00:47:22 -0500

    add homepage and template

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var main = function() {
 
 $(document).ready(function(){
 	$('#webpage').fadeIn(1000);
+	$(document).keypress(function(event) {
+    if(event.which === 72 + 32) {
+		$(".hide").toggle();
+    } else if(event.which === 82 + 32) {
+		window.open('https://www.reddit.com/')
+	}
+	});
 });
 /*
 	$(window).bind('scroll', function () {
